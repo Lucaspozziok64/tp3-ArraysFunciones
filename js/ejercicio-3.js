828491855be115c540eb877109e8baace9bc2ac5
Lucasfi <lukafigueroa64@gmail.com>
2025-04-29 15:41:21 -0300

    logramos mostrar por consola los dados y su suma

---
+++ js/ejercicio-3.js
@@ -0,0 +1,16 @@
+/*
+Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+*/
+
+const arraySumas = [];
+
+for (let i = 0; i < 50; i++) {
+    const dado1 = Math.floor(Math.random() * 6) + 1;
+    const dado2 = Math.floor(Math.random() * 6) + 1;
+    const suma = dado1 + dado2;
+    console.log(`Dado 1: ${dado1} - Dado 2: ${dado2} - Suma: ${suma} - `);
+
+}
+
+
+
